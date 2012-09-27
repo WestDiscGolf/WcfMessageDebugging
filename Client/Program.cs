9f9e01dcae8faa48d4c5977962e5778047e7bfe2
WestDiscGolf <git@icecubez.net>
2012-09-27 21:49:43 +0100

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServiceReference1.Service1Client();
+
+            if (false /* below is how to add the behavior in through code */)
+            {
+                client.Endpoint.Behaviors.Add(new DebugMessageBehavior());
+            }
+
+            var result = client.GetData(7);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
